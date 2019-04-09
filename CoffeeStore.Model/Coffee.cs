599a111b0b7e13a07391ab8e773bb333a6bf80e5
Cristóbal Tello <cristobal.tello@outlook.com>
2019-04-09 17:41:58 +0200

    Show INotifyPropertyChange and OneWay mode

--- CoffeeStore.Model/Coffee.cs
+++ CoffeeStore.Model/Coffee.cs
@@ -1,17 +1,141 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace CoffeeStore.Model
 {
-    public class Coffee
+    public class Coffee : INotifyPropertyChanged
     {
-        public int CoffeeId { get; set; }
-        public string CoffeeName { get; set; }
-        public int Price { get; set; }
-        public string Description { get; set; }
-        public Country OriginCountry { get; set; }
-        public bool InStock { get; set; }
-        public int AmountInStock { get; set; }
-        public DateTime FirstAddedToStockDate { get; set; }
-        public int ImageId { get; set; }
+        private int coffeeId;
+        private string coffeeName;
+        private int price;
+        private string description;
+        private Country originCountry;
+        private bool inStock;
+        private int amountInStock;
+        private DateTime firstAddedToStockDate;
+        private int imageId;
+
+        public int CoffeeId
+        {
+            get
+            {
+                return coffeeId;
+            }
+            set
+            {
+                coffeeId = value;
+                RaisePropertyChanged("CoffeeId");
+            }
+        }
+
+        public int Price
+        { 
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                RaisePropertyChanged("Price");
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+                RaisePropertyChanged("Description");
+            }
+        }
+
+        public Country OriginCountry
+        {
+            get
+            {
+                return originCountry;
+            }
+            set
+            {
+                originCountry = value;
+                RaisePropertyChanged("OriginCountry");
+            }
+        }
+
+        public bool InStock
+        {
+            get
+            {
+                return inStock;
+            }
+            set
+            {
+                inStock = value;
+                RaisePropertyChanged("InStock");
+            }
+        }
+
+        public int AmountInStock
+        {
+            get
+            {
+                return amountInStock;
+            }
+            set
+            {
+                amountInStock = value;
+                RaisePropertyChanged("AmountInStock");
+            }
+        }
+
+        public DateTime FirstAddedToStockDate
+        {
+            get
+            {
+                return firstAddedToStockDate;
+            }
+            set
+            {
+                firstAddedToStockDate = value;
+                RaisePropertyChanged("FirstAddedToStockDate");
+            }
+        }
+
+        public int ImageId
+        {
+            get
+            {
+                return imageId;
+            }
+            set
+            {
+                imageId = value;
+                RaisePropertyChanged("ImageId");
+            }
+        }
+
+        public string CoffeeName
+        {
+            get
+            {
+                return coffeeName;
+            }
+            set {
+                coffeeName = value;
+                RaisePropertyChanged("CoffeeName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
